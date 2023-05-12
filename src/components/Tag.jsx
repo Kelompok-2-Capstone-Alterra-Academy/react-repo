ccd650ea57a4a265eb5bee23d5a2bb7e75ec5fbf
Said Nizamudin <67599886+saidNizamudin@users.noreply.github.com>
2023-05-12 14:28:04 +0700

    feat: button and tag component (#1)

---
+++ src/components/Tag.jsx
@@ -0,0 +1,23 @@
+import styles from './Tag.module.css';
+import classNames from 'classnames/bind';
+
+export default function Tag({ type, children, className }) {
+	const colorStyle = (type) => {
+		switch (type) {
+			case 'Green':
+				return styles.green;
+			case 'Blue':
+				return styles.blue;
+			case 'Yellow':
+				return styles.yellow;
+			case 'Red':
+				return styles.red;
+			case 'Grey':
+				return styles.grey;
+			default:
+				return;
+		}
+	};
+
+	return <div className={classNames(styles.tag, colorStyle(type), className)}>{children}</div>;
+}
