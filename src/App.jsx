dfa5546295259afdd631a0c2d7d18e998ece8d5b
Said_Nizamudin <saidnizamudin@gmail.com>
2023-05-12 14:22:10 +0700

    feat: button and tag component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faTrash, faCirclePlus, faLink } from '@fortawesome/free-solid-svg-icons';
+import { Button, Tag } from './components';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className={styles.container}>
+			<div className={styles.primary}>
+				<Button className={styles.button} type="Primary">
+					<span>Simpan</span>
+				</Button>
+				<Button className={styles.button} type="Primary">
+					<span>Tambahkan</span>
+				</Button>
+				<Button className={styles.button} type="Primary">
+					<span>Berikan Quiz</span>
+				</Button>
+				<Button className={styles.button} type="Primary">
+					<span>Tambahkan Media</span>
+					<FontAwesomeIcon icon={faPlus} className={styles.iconRight} />
+				</Button>
+				<Button className={styles.button} type="Primary">
+					<FontAwesomeIcon icon={faCirclePlus} className={styles.iconLeft} />
+					<span>Tugaskan</span>
+				</Button>
+			</div>
+			<div className={styles.secondary}>
+				<Button className={styles.button} type="Secondary">
+					<FontAwesomeIcon icon={faPlus} />
+				</Button>
+				<Button className={styles.button} type="Secondary">
+					<span>Batal</span>
+				</Button>
+				<Button className={styles.button} type="Secondary">
+					<span>Hubungi</span>
+				</Button>
+				<Button className={styles.button} type="Secondary">
+					<FontAwesomeIcon icon={faLink} className={styles.iconLeft} />
+					<span>Link</span>
+				</Button>
+			</div>
+			<div className={styles.danger}>
+				<Button className={styles.button} type="Danger">
+					<FontAwesomeIcon icon={faTrash} />
+				</Button>
+				<Button className={styles.button} type="Danger">
+					<span>Hapus Siswa</span>
+				</Button>
+			</div>
+			<div className={styles.tagContainer}>
+				<Tag className={styles.tag} type="Green">
+					<span>Testing</span>
+				</Tag>
+				<Tag className={styles.tag} type="Blue">
+					<span>Testing</span>
+				</Tag>
+				<Tag className={styles.tag} type="Red">
+					<span>Testing</span>
+				</Tag>
+				<Tag className={styles.tag} type="Yellow">
+					<span>Testing</span>
+				</Tag>
+				<Tag className={styles.tag} type="Grey">
+					<span>Testing</span>
+				</Tag>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
