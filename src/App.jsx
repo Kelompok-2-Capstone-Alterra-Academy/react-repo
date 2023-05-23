69deb56842e3c4acd7619031dd17ea7461e83482
Said_Nizamudin <saidnizamudin@gmail.com>
2023-05-23 21:07:27 +0700

    Adding Outline Tag and Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,26 @@
 import styles from './App.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faTrash, faCirclePlus, faLink } from '@fortawesome/free-solid-svg-icons';
-import { Button, Tag } from './components';
+import { faPlus, faTrash, faLink } from '@fortawesome/free-solid-svg-icons';
+import { Button, Tag, OutlineTag, Profile } from './components';
 
 function App() {
 	return (
 		<div className={styles.container}>
-			<div className={styles.primary}>
+			<div className={styles.buttonContainer}>
 				<Button className={styles.button} type="Primary">
 					<span>Simpan</span>
-				</Button>
-				<Button className={styles.button} type="Primary">
-					<span>Tambahkan</span>
-				</Button>
-				<Button className={styles.button} type="Primary">
-					<span>Berikan Quiz</span>
 				</Button>
 				<Button className={styles.button} type="Primary">
 					<span>Tambahkan Media</span>
 					<FontAwesomeIcon icon={faPlus} className={styles.iconRight} />
 				</Button>
-				<Button className={styles.button} type="Primary">
-					<FontAwesomeIcon icon={faCirclePlus} className={styles.iconLeft} />
-					<span>Tugaskan</span>
-				</Button>
-			</div>
-			<div className={styles.secondary}>
 				<Button className={styles.button} type="Secondary">
 					<FontAwesomeIcon icon={faPlus} />
-				</Button>
-				<Button className={styles.button} type="Secondary">
-					<span>Batal</span>
-				</Button>
-				<Button className={styles.button} type="Secondary">
-					<span>Hubungi</span>
 				</Button>
 				<Button className={styles.button} type="Secondary">
 					<FontAwesomeIcon icon={faLink} className={styles.iconLeft} />
 					<span>Link</span>
 				</Button>
-			</div>
-			<div className={styles.danger}>
 				<Button className={styles.button} type="Danger">
 					<FontAwesomeIcon icon={faTrash} />
 				</Button>
@@ -64,6 +44,26 @@ function App() {
 				<Tag className={styles.tag} type="Grey">
 					<span>Testing</span>
 				</Tag>
+			</div>
+			<div className={styles.outlineTagContainer}>
+				<OutlineTag className={styles.outlineTag} type="Green">
+					<span>Testing</span>
+				</OutlineTag>
+				<OutlineTag className={styles.outlineTag} type="Blue">
+					<span>Testing</span>
+				</OutlineTag>
+				<OutlineTag className={styles.outlineTag} type="Red">
+					<span>Testing</span>
+				</OutlineTag>
+				<OutlineTag className={styles.outlineTag} type="Yellow">
+					<span>Testing</span>
+				</OutlineTag>
+				<OutlineTag className={styles.outlineTag} type="Grey">
+					<span>Testing</span>
+				</OutlineTag>
+			</div>
+			<div className={styles.otherContainer}>
+				<Profile name="Jennie BP" pic="src/assets/ava.jpg" email="jennieblpk20@email.com" />
 			</div>
 		</div>
 	);
