6934ab37eac9e02d90439606e7054acadd4fdde2
Said Nizamudin <67599886+saidNizamudin@users.noreply.github.com>
2023-05-25 01:04:06 +0700

    [Feature] Chat (Logo) and Set Up Indexing on Page and Components (#7)
    * feat: button and tag component
    
    * Adding Outline Tag and Profile
    
    * Slice prototype for UI implementation
    
    * Add logo and fix indexing on pages

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,3 +3,4 @@ export { default as Tag } from './Tag';
 export { default as OutlineTag } from './OutlineTag';
 export { default as Sidebar } from './Sidebar';
 export { default as Profile } from './Profile';
+export { default as NavBar } from './NavBar';
