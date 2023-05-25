599ecd7cac60ee982a145b3d6b60f3d75a5ffbd5
Kelompok2CapstAlterra <133635042+Kelompok2CapstAlterra@users.noreply.github.com>
2023-05-25 14:12:29 +0700

    [Fix] Indexing Component Folder (#9)
    * feat: button and tag component
    
    * Adding Outline Tag and Profile
    
    * Slice prototype for UI implementation
    
    * Add logo and fix indexing on pages
    
    * fix deployment build error
    
    * change logo path
    
    * fix indexing folder
    
    ---------
    
    Co-authored-by: Said_Nizamudin <saidnizamudin@gmail.com>

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,6 @@
-export { default as Button } from './Button';
-export { default as Tag } from './Tag';
-export { default as OutlineTag } from './OutlineTag';
-export { default as Sidebar } from './Sidebar';
-export { default as Profile } from './Profile';
-export { default as NavBar } from './NavBar';
+export * from './Button';
+export * from './Tag';
+export * from './OutlineTag';
+export * from './Sidebar';
+export * from './Profile';
+export * from './NavBar';
